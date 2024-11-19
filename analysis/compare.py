f9f5035dd4ed99666047e0e31b99af794bd9442b
Nithya Appannagaari <nithya@wifi-10-40-93-179.wifi.berkeley.edu>
2024-11-18 21:21:07 -0800

    python script for comparing - doesnt include comparing substrings of just the letters of the genes

---
+++ analysis/compare.py
@@ -0,0 +1,20 @@
+# given two JSON files, we want to compare the genes if cell_type is same and cell_type if genes are same
+import json
+
+file1 = 'evidence_emont.json'
+file2 = 'evidence_vijay.json'
+
+with open(file1, 'r') as f1, open(file2, 'r') as f2:
+    data1 = json.load(f1)
+    data2 = json.load(f2)
+
+
+for item1 in data1:
+    for item2 in data2:
+        if item1['cell_type'] == item2['cell_type'] or item1['gene'] == item2['gene'] or item1['gene'] in item2['gene'] or item2['gene'] in item1['gene']:
+            with open("similarities.txt", "a") as file:
+                file.write("Similarity Found\n")
+                file.write(json.dumps(item1))
+                file.write("\n")
+                file.write(json.dumps(item2))
+                file.write("\n\n")
